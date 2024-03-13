14988e0b64191a8f7ad70477ac9c6b092b858b24
ashrafalmayah <ashrafalmayah00@gmail.com>
2024-03-13 16:22:51 +0300

    collection sort and caching

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -19,17 +19,17 @@ class Post
 
     public static function all()
     {
-        $files = File::files(resource_path("posts"));
-
-        return collect($files)
-            ->map(fn ($file) => YamlFrontMatter::parseFile($file))
-            ->map(fn ($document) => new Post(
-                $document->title,
-                $document->excerpt,
-                $document->date,
-                $document->body(),
-                $document->slug
-            ));
+        return cache()->rememberForever('posts.all', function () {
+            return collect(File::files(resource_path("posts")))
+                ->map(fn ($file) => YamlFrontMatter::parseFile($file))
+                ->map(fn ($document) => new Post(
+                    $document->title,
+                    $document->excerpt,
+                    $document->date,
+                    $document->body(),
+                    $document->slug
+                ))->sortByDesc('date');
+        });
     }
 
     public static function find($slug)
