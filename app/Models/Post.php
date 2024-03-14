47a6cd05e417c36b0499162d735edaf2db5edf72
ashrafalmayah <ashrafalmayah00@gmail.com>
2024-03-14 13:21:38 +0300

    introduce find or fail method

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -36,4 +36,15 @@ class Post
     {
         return static::all()->firstWhere(fn ($post) => $post->slug == $slug);
     }
+
+    public static function findOrFail($slug)
+    {
+        $post = static::find($slug);
+
+        if(! $post){
+            throw new ModelNotFoundException;
+        }
+
+        return $post;
+    }
 }
