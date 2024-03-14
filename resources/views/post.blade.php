e0c07fcd46361273600d62d517917f04c430a56d
ashrafalmayah <ashrafalmayah00@gmail.com>
2024-03-14 13:19:47 +0300

    use blade instead of normal php

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,22 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/css/main.css">
-    <title>Blog | <?= $post->title ?></title>
-</head>
-
-<body>
-    <main class="container">
-        <article class="post-item">
-            <h1><?= $post->title ?></h1>
-            <?= $post->body ?>
-        </article>
-        <a href="/">Go Back</a>
-    </main>
-</body>
-
-</html>
+<x-layout title="Blog | {{ $post->title }}">
+    <article class="post-item">
+        <h1>{{ $post->title }}</h1>
+        {!! $post->body !!}
+    </article>
+    <a href="/">Go Back</a>
+</x-layout>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,26 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/css/main.css">
-    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-    <title>Posts</title>
-</head>
-
-<body>
-    <main class="container">
-        <?php foreach($posts as $post): ?>
+<x-layout title="My blog">
+    @foreach ($posts as $post)
         <div class="post-item">
-            <a href="/posts/<?= $post->slug ?>"><?= $post->title ?></a>
+            <a href="/posts/{{ $post->slug }}">{{ $post->title }}</a>
             <p>
-                <?= $post->excerpt ?>
+                {{ $post->excerpt }}
             </p>
         </div>
-        <?php endforeach; ?>
-    </main>
-</body>
-
-</html>
+    @endforeach
+</x-layout>
