1812ebd9a7aaec0f1829ee346c283928295558d4
ashrafalmayah <ashrafalmayah00@gmail.com>
2024-03-14 16:12:06 +0300

    use eloquent relationships for post categories

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,6 +1,10 @@
 <x-layout title="Blog | {{ $post->title }}">
     <article class="post-item">
         <h1>{{ $post->title }}</h1>
+        <div class="category-list">
+            Categories:
+            <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
+        </div>
         <p>
             {{ $post->body }}
         </p>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,7 +1,11 @@
 <x-layout title="My blog">
     @foreach ($posts as $post)
         <div class="post-item">
-            <a href="/posts/{{ $post->slug }}">{{ $post->title }}</a>
+            <h1><a href="/posts/{{ $post->slug }}">{{ $post->title }}</a></h1>
+            <div class="category-list">
+                Categories:
+                <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
+            </div>
             <p>
                 {{ $post->excerpt }}
             </p>
