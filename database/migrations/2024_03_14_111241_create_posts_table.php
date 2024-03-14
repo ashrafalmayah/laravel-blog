151679eaf03376c4fbbbe123dec22951e5c2a438
ashrafalmayah <ashrafalmayah00@gmail.com>
2024-03-14 15:30:41 +0300

    get post by route model binding

--- database/migrations/2024_03_14_111241_create_posts_table.php
+++ database/migrations/2024_03_14_111241_create_posts_table.php
@@ -13,9 +13,11 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('slug')->unique();
             $table->string('title');
             $table->text('excerpt');
-            $table->text('body');            $table->timestamp('published_at')->nullable();
+            $table->text('body');
+            $table->timestamp('published_at')->nullable();
             $table->timestamps();
         });
     }
