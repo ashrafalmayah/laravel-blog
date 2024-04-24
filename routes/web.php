44e7f37abcf6f43258bcab559b3606dfbf575f29
ashrafalmayah <ashrafalmayah00@gmail.com>
2024-04-24 18:53:55 +0300

    admin middleware

--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,6 @@ Route::post('register', [RegisterController::class, 'store'])->middleware('guest
 Route::get('login', [SessionsController::class, 'create'])->middleware('guest')->name('login');
 Route::post('login', [SessionsController::class, 'store'])->middleware('guest');
 Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth');
+
+Route::get('admin/posts/create', [PostController::class, 'create'])->middleware('admin');
+Route::post('admin/posts', [PostController::class, 'store'])->middleware('admin');
